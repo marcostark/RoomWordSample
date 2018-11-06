613a7cebda4c9ed2e51e594a47f118b70f844653
riansco14 <riantal@gmail.com>
2018-11-06 15:00:01 -0300

    Criando classes DAO, repository, ViewModel, Database

---
+++ app/src/main/java/com/gqdostark/roomwordsample/WordRepository.java
@@ -0,0 +1,49 @@
+package com.gqdostark.roomwordsample;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class WordRepository {
+    /**
+     * - Classe responsavel por abstrair o acesso as diversas fonte de dados
+     * - Lida com operações de dados, fornecendo uma API limpa para o restante
+     * da aplicação para os dados da mesma
+     * - Gerencia tópicos de consulta e permite usar vários abck-ends
+     * */
+
+    private WordDao mWordDao;
+    private LiveData<List<Word>> mAllWords;
+
+    /* Construtor que obtem identificado para o BD e inicializa as variáveis*/
+    WordRepository(Application application) {
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords(){
+        return mAllWords;
+    }
+
+    public void insert(Word word){
+        new insertAsyncTask(mWordDao).execute(word);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Word, Void, Void> {
+
+        private WordDao mAsyncTaskDao;
+
+        insertAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        protected Void doInBackground(final Word... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/gqdostark/roomwordsample/WordViewModel.java
@@ -0,0 +1,13 @@
+package com.gqdostark.roomwordsample;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public class WordViewModel extends AndroidViewModel {
+
+
+    public WordViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
